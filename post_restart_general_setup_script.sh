f8e51231e5861c6442d89daf16756c6c3702cb75
Marco <41395992+ribeirompl@users.noreply.github.com>
2020-01-20 23:01:30 +0200

    See last commit

---
+++ post_restart_general_setup_script.sh
@@ -0,0 +1,5 @@
+# can it be run immediately after installing vs code?
+code --install-extension Shan.code-settings-sync
+
+# Open code and login with github, or maybe set up the gist info behind the scenes?
+
